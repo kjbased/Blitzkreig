6b9ad9cc7bc38d3f730e9f5a06f119cff18752de
KJ <kjbass@outlook.com>
2025-06-01 20:01:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+axisVP = 0
+alliedVP = 0
+japanVP = 0
+westernEuropeTrack = 0
+easternEuropeTrack = 0
+africaTrack = 0
+pacificTrack = 0
+southEastAsiaTrack = 0
+
+activePlayer = "Axis"
+lastUsedTroop = []
+strategems = []
+
 def main():
 
 main()
