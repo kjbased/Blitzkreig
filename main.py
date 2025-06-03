7097bb254d1a153b90a559902470d4fc2c34f3af
KJ <kjbass@outlook.com>
2025-06-02 23:36:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+from StartingVariables import *
+
 axisVP = 0
 alliedVP = 0
 japanVP = 0
@@ -7,12 +10,52 @@ africaTrack = 0
 pacificTrack = 0
 southEastAsiaTrack = 0
 
-startingAxis = startingTroops
-startingAllies = startingTroops
+axisTiles = startingTroops
+alliesTiles = startingTroops
+researchTiles = startingResearch
+
+alliedReserves = []
+axisReserves = []
 
 activePlayer = "Axis"
 lastUsedTroop = []
 strategems = []
+
+random.shuffle(axisTiles)
+random.shuffle(alliesTiles)
+random.shuffle(researchTiles)
+
+'''
+def pop(self):
+  if len(self.items) == 0:
+    return None
+  item = self.items[-1]
+  del self.items[-1]
+  return item
+'''
+def research():
+  if len(researchTiles) == 0:
+    return None
+  item = researchTiles[-1]
+  if activePlayer == "Axis":
+    axisTiles.append(item)
+  if activePlayer == "Allies":
+    alliesTiles.append(item)
+  del researchTiles[-1]
+  
+def industry():
+  if axisTiles == 0:
+    return None
+  item = 
+  if activePlayer == "Axis":
+    
+def bombard():
+
+def blitz():
+
+def draw2reserves ():
+  item
+  
 
 def main():
 
